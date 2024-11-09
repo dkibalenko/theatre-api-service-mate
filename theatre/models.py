64712f1e9368e0fb5fc7c71d412b95bbc9f26c2b
Denys Kibalenko <dennieking87@gmail.com>
2024-11-09 18:03:31 +0100

    Feat: add TheatreHall model to theatre/models.py

--- theatre/models.py
+++ theatre/models.py
@@ -47,4 +47,16 @@ class Play(models.Model):
 
     def __str__(self):
         return self.title
- 
+    
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
