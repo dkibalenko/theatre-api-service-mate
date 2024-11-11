737cdc717f14be6e7e5867a7855bebd7b4a79cd6
Denys Kibalenko <dennieking87@gmail.com>
2024-11-11 19:07:23 +0100

    Feat: add PlaySerializer, PlayListSerializer, PlayDetailSerializer, PlayImageSerializer to theatre/serializers.py

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from theatre.models import Actor, Genre
+from theatre.models import Actor, Genre, Play
 
 
 class ActorSerializer(serializers.ModelSerializer):
@@ -13,3 +13,55 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
+
+
+class PlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "genre", "actors")
+
+
+class PlayListSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="full_name",
+    )
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+            "image",
+        )
+
+
+class PlayDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+            "image",
+        )
+
+
+class PlayImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = ("id", "image")
