0a3601dd245c32593afa843cf4eb175c8aee814e
Denys Kibalenko <dennieking87@gmail.com>
2024-11-11 17:47:19 +0100

    Feat: add ActorViewSet, GenreViewSet to theatre/views.py

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import Actor, Genre
+from theatre.serializers import ActorSerializer, GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
