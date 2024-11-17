5f1ab3da02ed08c269499f087fa63b8af1931bd0
Denys Kibalenko <dennieking87@gmail.com>
2024-11-17 13:52:11 +0100

    Feat: add JWT authentication
    - Add JWTAuthentication class as DEFAULT_AUTHENTICATION_CLASSES for the whole applicaton to theatre_service/settings.py
    - Add SIMPLE_JWT basic settings to theatre_service/settings.py
    - Add url routes for TokenObtainPairView, TokenRefreshView, TokenVerifyView to user/urls.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +135,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+	"REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+	"ROTATE_REFRESH_TOKENS": False
+}
