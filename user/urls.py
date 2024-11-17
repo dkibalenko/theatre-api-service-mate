7ad451bd3963b195dc0fd81c4e93f153f55096a8
Denys Kibalenko <dennieking87@gmail.com>
2024-11-17 19:07:13 +0100

    Feat: add ManageUserView to user/views.py
    - Add update method to UserSerializer to user/serializers.py for updating user with encypted password
    - Add ManageUserView to user/views.py with TokenAuthentication, IsAuthenticated classes
    - Add url route for ManageUserView to user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from user.views import CreateUserView, LoginUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 
 
 app_name = "user"
@@ -15,6 +15,7 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
     path(
         "token/",
         TokenObtainPairView.as_view(),
