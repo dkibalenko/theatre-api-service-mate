ca2551aabd7c3d37429327628b3cb50631cd649f
Denys Kibalenko <dennieking87@gmail.com>
2024-11-19 11:50:18 +0100

    Feat: add PropInline inline class to include it to PerformanceAdmin class to display properties related to Performance instance in the admin site in theatre/admin.py

--- theatre/admin.py
+++ theatre/admin.py
@@ -13,9 +13,23 @@ from theatre.models import (
 
 admin.site.register(Actor)
 admin.site.register(Genre)
-admin.site.register(Performance)
 admin.site.register(Play)
 admin.site.register(Prop)
 admin.site.register(TheatreHall)
 admin.site.register(Reservation)
 admin.site.register(Ticket)
+
+
+class PropInline(admin.TabularInline):
+    model = Prop.performance.through
+    extra = 0
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    inlines = [PropInline]
+    list_display = ("play", "theatre_hall", "show_time", "prop_list", "id")
+    
+    def prop_list(self, obj):
+        return ", ".join([prop.name for prop in obj.props.all()])
+    
+    prop_list.short_description = "Properties"
