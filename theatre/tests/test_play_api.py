657c97b36c89a322c81a86ec8b536c8b88ef479a
Denys Kibalenko <dennieking87@gmail.com>
2024-11-21 18:33:46 +0100

    Test: add test post image to play list url and image is not saved

--- theatre/tests/test_play_api.py
+++ theatre/tests/test_play_api.py
@@ -99,3 +99,25 @@ class PlayImageUploadTest(TestCase):
         res = self.client.post(url, {"image": "not image"}, format="multipart")
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_image_to_play_list(self):
+        url = PLAY_LIST_URL
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {
+                    "title": "Title",
+                    "description": "Description",
+                    "genres": [1],
+                    "actors": [1],
+                    "image": ntf,
+                },
+                format="multipart",
+            )
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        play = Play.objects.get(title="Title")
+        self.assertFalse(play.image)
