ad37be8e222f05b030224f1be87ecc64218ee391
Denys Kibalenko <dennieking87@gmail.com>
2024-11-25 16:51:55 +0100

    Test: add theatre/tests/test_ticket_api.py with AuthenticatedTicketApiTests with test validate ticket static method of Ticket model

---
+++ theatre/tests/test_ticket_api.py
@@ -0,0 +1,62 @@
+from django.utils import timezone
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+from rest_framework.test import APIClient
+
+from theatre.models import Play, Ticket, Performance, Reservation, TheatreHall
+
+
+class AuthenticatedTicketApiTests(TestCase):
+    """Test the API for tickets"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email="test@test.com",
+            password="testpassword"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+        self.theatre_hall = TheatreHall.objects.create(
+            name="Main Hall",
+            rows=10,
+            seats_in_row=20
+        )
+        self.play = Play.objects.create(
+            title="Example Play",
+            description="An example play description.",
+        )
+        self.performance = Performance.objects.create(
+            play=self.play,
+            theatre_hall=self.theatre_hall,
+            show_time=timezone.now()
+        )
+        self.reservation = Reservation.objects.create(user=self.user)
+
+    def test_validate_ticket(self):
+        # Valid ticket
+        try:
+            Ticket.validate_ticket(
+                row=5,
+                seat=10,
+                theatre_hall=self.theatre_hall,
+                error_to_raise=ValidationError
+            )
+        except ValidationError:
+            self.fail("ValidationError raised unexpectedly for valid ticket.")
+        # Invalid row
+        with self.assertRaises(ValidationError) as cm:
+            Ticket.validate_ticket(11, 10, self.theatre_hall, ValidationError)
+            self.assertIn(
+                "row number must be in available range",
+                str(cm.exception)
+            )
+        # Invalid seat
+        with self.assertRaises(ValidationError) as cm:
+            Ticket.validate_ticket(5, 21, self.theatre_hall, ValidationError)
+            self.assertIn(
+                "seat number must be in available range",
+                str(cm.exception)
+            )
