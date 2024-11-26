2dcd155ed578aaa4f5a9d425c47c9f908cef11e1
Denys Kibalenko <dennieking87@gmail.com>
2024-11-26 19:43:55 +0100

    Chore: switch to JWT authenticaton, set up settings to use JWTAuthentication

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -140,8 +140,8 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        # "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
