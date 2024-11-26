ec6b2d9588c8ff76b8cade97c7920d05739d8b3e
Denys Kibalenko <dennieking87@gmail.com>
2024-11-26 17:42:19 +0100

    Test: add UserSerializerTests class with test create user and test update user to theatre/tests/test_user.py

--- user/tests/test_user.py
+++ user/tests/test_user.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from rest_framework.test import APITestCase
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
 
 class UserTests(TestCase):
     def test_create_user_without_email_should_raise_value_error(self):
@@ -37,3 +41,36 @@ class UserTests(TestCase):
             str(cm.exception),
             "Superuser must have is_superuser=True."
         )
+
+
+class UserSerializerTests(APITestCase):
+    def test_create_user(self):
+        payload = {
+            "email": "test@test.com",
+            "password": "testpassword"
+        }
+        serializer = UserSerializer(data=payload)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", serializer.data)
+        self.assertEqual(user.email, payload["email"])
+
+    def test_update_user(self):
+        user = get_user_model().objects.create_user(
+            email="test@test.com",
+            password="testpassword"
+        )
+        payload = {
+            "email": "test2@test.com",
+            "password": "testpassword2"
+        }
+        serializer = UserSerializer(
+            instance=user,
+            data=payload,
+            partial=True
+        )
+        self.assertTrue(serializer.is_valid())
+        updated_user = serializer.save()
+        self.assertTrue(updated_user.check_password(payload["password"]))
+        self.assertEqual(updated_user.email, payload["email"])
