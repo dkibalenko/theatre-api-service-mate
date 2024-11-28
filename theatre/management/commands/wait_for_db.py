fbcc723f3cd58c207119efeef8e16080de974f3c
Denys Kibalenko <dennieking87@gmail.com>
2024-11-28 11:10:14 +0100

    Feat: add python manage.py wait_for_db command to docker-compose.yml
    - Add docstring to Command class handle method to theatre/management/commands/wait_for_db.py

--- theatre/management/commands/wait_for_db.py
+++ theatre/management/commands/wait_for_db.py
@@ -8,6 +8,14 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
+        """
+        Handle the command.
+
+        This command is used in the docker-entrypoint.sh file to
+        wait for the database to be available before running the
+        collectstatic and runserver commands. If the database is not
+        available the command will wait a second and try again.
+        """
         self.stdout.write("Waiting for database...")
         db_conn = None
         while not db_conn:
