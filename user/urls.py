bc627a00fa96d4fd0363e951598bdd69cb97c938
Denys Kibalenko <dennieking87@gmail.com>
2025-06-16 15:17:49 +0200

    Fix: remove url pattern `login/` for `LoginUserView` view out of user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -7,14 +7,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from user.views import CreateUserView, LoginUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", LoginUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path(
         "token/",
